2b4c33e06bd8e8c59ff72f1e01bc5f0e00082b6e
Guaziiii <GuaziTech@yeah.net>
2022-09-22 21:58:21 +0800

    Site updated: 2022-09-22 21:58:21

---
+++ js/diytitle.js
@@ -0,0 +1,18 @@
+//动态标题
+var OriginTitile = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        //离开当前页面时标签显示内容
+        document.title = '在想什么呢？';
+        clearTimeout(titleTime);
+    }
+    else {
+        //返回当前页面时标签显示内容
+        document.title = '有答案了吗！' + OriginTitile;
+        //两秒后变回正常标题
+        titleTime = setTimeout(function () {
+            document.title = OriginTitile;
+        }, 2000);
+    }
+});
